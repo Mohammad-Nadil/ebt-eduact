9ab3287d4a8399e76174349fd6e61a6651f57989
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-13 19:52:06 +0600

    npm intall

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const Pricing = () => {
   return (
     <div>
-      <h1 className="text-7xl">Pricing</h1>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Pricing;
+export default Pricing
