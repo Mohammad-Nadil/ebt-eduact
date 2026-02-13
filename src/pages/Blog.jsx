132bf1c217616af6ead23e3f13a9b76a16cda734
sabrinaahamed <sabrinaahamed.official@gmail.com>
2026-02-13 21:30:11 +0600

    blog banner part

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Container from "../components/Container";
 
 const Blog = () => {
   return (
     <div>
-      <h1 className="text-7xl">Blog</h1>
-       <NavLink
-          className={({ isActive }) =>
-            isActive ? "text-red-500" : "text-black"
-          }
-          to="/blog/:id"
-        >
-          <p>BlogDetail</p>
-        </NavLink>
+
+      <div className="bg-white py-[50px]"></div>
+      <div className="bg-black py-[147px]">
+        <Container>
+          <div className="text-center">
+            <h1 className="text-white text-7xl font-bold mb-4">BLOG</h1>
+            <div className="flex items-center justify-center gap-2 text-white">
+              <NavLink to="/" className="hover:text-secondary">
+                Home
+              </NavLink>
+              <span>/</span>
+              <span>Blog</span>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 };
