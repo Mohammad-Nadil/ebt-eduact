e23cc330f08d48771c444c549af042504406eab9
sabrinaahamed <sabrinaahamed.official@gmail.com>
2026-02-14 14:56:41 +0600

    Refactor BlogDetails component layout and styling

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Container from "../components/Container";
+import { NavLink } from "react-router-dom";
 
 const BlogDetails = () => {
   return (
     <div>
-      <h1 className="text-7xl">BlogDetails</h1>
+      {/* Banner */}
+      <div className="bg-black pt-20 pb-32">
+        <Container>
+          <h1 className="text-white text-center text-7xl font-bold mb-4">BLOG DETAILS</h1>
+          <p className="text-white text-center">
+            <NavLink to="/">Home</NavLink> / <span>Blog</span>
+          </p>
+
+        </Container>
+      </div>
+
+      
     </div>
   );
 };
