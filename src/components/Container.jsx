1100849c6a67317e5ec552abd4cfd9cc7004bc35
Mohammed Hossainur Rashid Nadil <mdhossain.dev@gmail.com>
2026-02-16 11:10:06 +0600

    Add style prop to Container component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Container = ({ children, className }) => {
+const Container = ({ children, className , style }) => {
   return <div style={style} className={`max-w-330 mx-auto relative px-4 sm:px-6 lg:px-12 font-urbanist ${className}`}>{children}</div>;
 };
 
